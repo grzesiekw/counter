d084fdec2bb3cd744343e0222dcc8ce292ec8132
grzesiekw <g.wilkowicz@gmail.com>
2015-09-12 21:03:05 +0200

    Counter - HTTP API, move configuration to config file

--- counter-app/src/main/scala/counter/CounterApp.scala
+++ counter-app/src/main/scala/counter/CounterApp.scala
@@ -4,6 +4,7 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
+import com.typesafe.config.ConfigFactory
 import counter.http.{ActorCounterService, CounterApi}
 import counter.manager.CounterManager
 import counter.operation.OperationReceiver
@@ -13,17 +14,19 @@ import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 
 object CounterApp extends App with CounterApi with ActorCounterService {
+  val config = ConfigFactory.load()
+
   implicit val system = ActorSystem("counter")
   implicit val materializer = ActorMaterializer()
 
   val executionContext: ExecutionContext = system.dispatcher
-  val operationTimeout: Timeout = 200 millis
+  val operationTimeout: Timeout = config.getLong("api.timeout") millis
 
   val warningCollector = system.actorOf(WarningCollector.props())
   val counterManager = system.actorOf(CounterManager.props(warningCollector))
   val operationReceiver = system.actorOf(OperationReceiver.props(counterManager))
 
-  val binding = Http().bindAndHandle(route, "localhost", 8080)
+  val binding = Http().bindAndHandle(route, config.getString("api.host"), config.getInt("api.port"))
 
   println("Counter is up and running")
 
